f24dff01330e189d294a739ea0922cd838d2c8ce
TKHatton <126082633+TKHatton@users.noreply.github.com>
2025-08-24 01:58:02 +0000

    Removed user_bp

--- src/main.py
+++ src/main.py
@@ -6,7 +6,6 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 from flask import Flask, send_from_directory
 from flask_cors import CORS
 from src.models.user import db
-from src.routes.user import user_bp
 from src.routes.voting import voting_bp
 
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
@@ -15,7 +14,6 @@ app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
 # Enable CORS for all routes
 CORS(app)
 
-app.register_blueprint(user_bp, url_prefix='/api')
 app.register_blueprint(voting_bp, url_prefix='/api')
 
 # uncomment if you need to use database
